24ae034ed67fdfb83dc22a63f6dfe666c5f5eeb1
Nattanon Yanil <nattanonyanil@gmail.com>
2018-07-20 09:25:54 +0700

    20/07/2018 - 09:25

--- keyword crawler/main.py
+++ keyword crawler/main.py
@@ -4,14 +4,10 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import datetime
 from arxiv.arxiv import *
-from acs.acs import *
 from scienceDirect.scienceDirect import *
 from wiley.wiley import *
 from springer.springer import *
-from sagepub.sagepub import *
-from researchgate.researchgate import *
 from europePMC.europePMC import *
-
 def craw(key,name,choice):
     print("start key", key)
     if(choice == "Wiley"):
@@ -27,15 +23,3 @@ def craw(key,name,choice):
         scienceDirect(key,name)
     elif(choice == "arXiv"):
         arXiv(key,name)
-    # elif(choice == "5"):
-    #     key = input("Enter the keyword : ")
-    #     name = input("Enter file name : ")
-    #     sage(key,name)
-    # elif(choice == "6"):
-    #     key = input("Enter the keyword : ")
-    #     name = input("Enter file name : ")
-    #     gate(key,name)
-    # elif(choice == "6"):
-    #     key = input("Enter the keyword : ")
-    #     name = input("Enter file name : ")
-    #     acs(key,name)
